2f5f62b372927809c43f0a167c4a0b1d5e51808b
Arthur <74686290+aferrande@users.noreply.github.com>
2022-12-07 05:26:50 -0300

    Add files via upload

---
+++ Projeto-de-pr-tica-curso-React---The-Complete-Guide-na-udemy-master/src/components/UI/Button.js
@@ -0,0 +1,19 @@
+import React from "react";
+import css from "./Button.module.css";
+
+const Button = (props) => {
+
+    
+
+  return (
+    <button
+      type={props.type || "buton"}
+      className={css.button}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
